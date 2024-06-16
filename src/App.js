23a2b919d9e1dea033a3d124b9beb4ebad3b3340
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-06-16 18:00:35 +0300

    imported the code of the whole weathersearch.js app from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./Weather";
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Well hello there!</p>
-        <Weather />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WeatherSearch />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Weather from "./Weather";
+import Weather from "./WeatherSearch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Weather />
   </React.StrictMode>
 );
 
