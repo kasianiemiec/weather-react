c5b82bd6035ba5b3da26f36c366948227a531119
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-06-16 18:49:10 +0300

    added babelrc

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <WeatherSearch />
+
+        <p>
+          This project was coded by Kasia Niemiec and is open-sourced on{" "}
+          <a
+            href="https://github.com/kasianiemiec/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://jocular-licorice-aaa95d.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
       </header>
     </div>
   );
